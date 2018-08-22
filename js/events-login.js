02033ca1a71a3bdd0961239426fca1101d91a3d8
MamuteLucas <lucaspmazutti@gmail.com>
2018-08-22 00:28:47 -0300

    Melhorada a funcao de login

--- js/events-login.js
+++ js/events-login.js
@@ -9,8 +9,6 @@ function submitLogin(){
       window.location = "home.php";
     } else if(numRows == 0 && correctInfo){
       correctInfo = false;
-      $("input[name = 'login_password']").css({margin: "0.8rem 0 0.2rem 0"});
-      $("#form_inputButton").css("margin", "0.28rem 0 0.8rem 0");
       $("#div_confirmPassword").append("<small id='small_login'>E-mail ou senha incorretos!</small>");
     }
   });
